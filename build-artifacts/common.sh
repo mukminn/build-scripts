7c9b8f94d608369fbbcf008da2b347cd7d760ba4
Miao ZhiCheng <hellwolf.misty@gmail.com>
2021-05-21 10:41:53 +0300

    normalizeBranchName 50 chars max

--- build-artifacts/common.sh
+++ build-artifacts/common.sh
@@ -8,5 +8,5 @@ function normalizeBranchName {
     # 1. change dots to underscores
     # 2. to lower case
     # 3. cut to maximum 80 characters
-    echo $(basename "$1" | sed 's/\./_/g' | tr '[:upper:]' '[:lower:]' | cut -c -80)
+    echo $(basename "$1" | sed 's/\./_/g' | tr '[:upper:]' '[:lower:]' | cut -c -50)
 }
